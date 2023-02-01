32ac59a59e0ee10299c5894c526daa2473d2c077
Jander Laffita <janderdeveloper@gmail.com>
2023-02-01 11:31:12 -0500

    se prepara la activity para contener google Map

--- app/src/main/java/com/example/mapandlocationspractices/MainActivity.kt
+++ app/src/main/java/com/example/mapandlocationspractices/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.mapandlocationspractices
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+// Se implementa la interfaz OnMapReadyCallBack
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var mapFragment: SupportMapFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Se obtiene la referencia del mapFragment
+        mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        // Se obtiene el manejador del fragment
+        mapFragment.getMapAsync(this)
+    }
+
+    // Se obtiene el manejador del googe map
+    override fun onMapReady(googleMap: GoogleMap) {
+
     }
 }
